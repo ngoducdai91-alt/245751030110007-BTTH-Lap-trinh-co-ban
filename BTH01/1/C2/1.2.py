6c66d5628d5a75e13b62ef7d68b53109d039c587
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-10 20:42:18 +0700

    CODE

---
+++ BTH01/1/C2/1.2.py
@@ -0,0 +1,12 @@
+print("NGO DUC DAI")
+print("msv 245751030110007")
+print("2)")
+import math;
+x1=int(input("nhap tu ban phim x1--->"))
+y1=int(input("nhap tu ban phim y1--->"))
+x2=int(input("nhap tu ban phim x2--->"))
+y2=int(input("nhap tu ban phim y2--->"))
+d1=(x2-x1)*(x2-x1); 
+d2=(y2-y1)*(y2-y1);
+res=math.sqrt(d1+d2)
+print("khoang cach giua hai diem:",res);
