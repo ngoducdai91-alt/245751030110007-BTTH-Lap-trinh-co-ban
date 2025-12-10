5ca8c80af450d5cd2e5562f9feb28346734602cd
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-10 21:03:12 +0700

    CODE

---
+++ BTH01/1/C8/1.8.py
@@ -0,0 +1,12 @@
+print("NGO DUC DAI")
+print("msv 245751030110007")
+print("8)")
+a,b=1,2
+total=0
+print(a,end=" ")
+while(a<=4000000-1):
+    print(a,end=" ")
+    if a%2==0:
+        total +=a
+    a,b=b,a+b
+print("\n tong cac so chan trong day Fibonnaci:",total)
