13dea089a2436314b9799a70e8fede323d9bb292
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-10 21:17:18 +0700

    CODE

---
+++ BTH01/1/CAU11/1.11.py
@@ -0,0 +1,11 @@
+print("NGO DUC DAI")
+print("msv 245751030110007")
+print("11)")
+l=[1,"python",4,7]
+k=['cse',2,'guntur',8]
+m=[]
+m.append(l);
+m.append(k);
+print(m)
+d={1:l,2:k,'combine_list':m}
+print(d)
