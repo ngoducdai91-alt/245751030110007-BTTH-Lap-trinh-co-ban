d91df034b62294d2f607ca9033246ea1163338d0
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-10 20:51:35 +0700

    CODE

---
+++ BTH01/1/C4/1.4.py
@@ -0,0 +1,10 @@
+print("NGO DUC DAI")
+print("msv 245751030110007")
+print("4)")
+a=int(input("nhap a tu ban phim"))
+b=int(input("nhap b tu ban phim"))
+i=1;
+for j in range(a,b):
+    print("i:",i,"j:",j)
+    print(i,"/",j)
+    print(i/j);
