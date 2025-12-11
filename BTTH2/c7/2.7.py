2023a6e39c6c294a656364e45e74491c43ab6037
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-11 18:27:34 +0700

    code

---
+++ BTTH2/c7/2.7.py
@@ -0,0 +1,10 @@
+print("NGÔ ĐỨC ĐẠI")
+print("msv:245751030110007")
+print("7)")
+n=int(input("Nhap so:"))
+def Kiem_tra_so(n):
+ if n%2 == 0:
+  print ("Day là so chan")
+ else:
+  print ("Day la so le")
+Kiem_tra_so(n)
