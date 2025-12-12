9f7b3fb234093fdeced5484b665894821937b091
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-12 15:25:56 +0700

    code

---
+++ BTTH3/c10/3.10.py
@@ -0,0 +1,10 @@
+print("NGÔ ĐỨC ĐAI")
+print("msv:245751030110007")
+print("10)")
+print("Chuỗi bài 9 sau khi bỏ phần đầu và cuối:")
+
+ds = "ngoducdai"   # Khai báo chuỗi
+x = ds[1:-1]       # Bỏ ký tự đầu và cuối
+
+for c in x:
+    print(c)
