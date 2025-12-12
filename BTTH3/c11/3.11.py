faa502b93dd6e1e4ed0d2a24194d4d5c1373356b
ngoducdai91-alt <ngoducdai91@gmail.com>
2025-12-12 16:09:06 +0700

    code

---
+++ BTTH3/c11/3.11.py
@@ -0,0 +1,9 @@
+print("NGÔ ĐỨC ĐAI")
+print("msv:245751030110007")
+print("11)")
+ds=input("Nhập chuỗi:").split()
+print("Chuỗi bài 9 sau khi thêm abc và 123 cuối chuỗi:")
+ds.append("abc")
+ds.append("123")
+for ch in ds:
+    print(ch)
